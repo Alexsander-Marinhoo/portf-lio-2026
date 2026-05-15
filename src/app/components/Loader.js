83a7374242bbef3cf75797fb3dcbc076cefaf425
Alexsander-Marinhoo <marinhoalexsander9@gmail.com>
2026-05-15 12:23:18 -0300

    fix: adjust mobile layout and responsive images on project page

--- src/app/components/Loader.js
+++ src/app/components/Loader.js
@@ -1,10 +1,10 @@
 "use client";
 import { gsap } from "gsap";
-import { useRef , useEffect , useState } from "react";
-export default function Loader(){
+import { useRef, useEffect, useState } from "react";
+export default function Loader() {
 
     const [showLoader, setShowLoader] = useState(false);
-    
+
     //refs para elementos do DOM
     const loaderRef = useRef(null);
     const blockRefs = useRef([])
@@ -14,7 +14,7 @@ export default function Loader(){
     const digitRefs = useRef([]);
 
     //valores para carregar no DOM
-    const digitsCount = [[0,0] , [2,7] , [6,5] , [9,8] , [9,9] , ['A','L','E','X']];
+    const digitsCount = [[0, 0], [2, 7], [6, 5], [9, 8], [9, 9], ['A', 'L', 'E', 'X']];
     const blocksValue = [[]];
 
     useEffect(() => {
@@ -37,19 +37,19 @@ export default function Loader(){
 
         const tl = gsap.timeline({
             delay: 0.3,
-            defaults:{
+            defaults: {
                 ease: "expo.out",
             }
         });
-        
-        if(showLoader){
+
+        if (showLoader) {
             window.scrollTo(0, 0);
             document.body.style.overflow = 'hidden';
         }
 
 
 
-        countRefs.current.forEach((item , index) => {
+        countRefs.current.forEach((item, index) => {
             const h1Arrays = Array.from(item.querySelectorAll('div h1'));
 
             tl.to(h1Arrays, {
@@ -58,12 +58,12 @@ export default function Loader(){
                 stagger: 0.075
             }, index * 1)
 
-            if(index < countRefs.current.length) {
+            if (index < countRefs.current.length) {
                 tl.to(h1Arrays, {
-                y: '-100%',
-                duration: 1,
-                stagger: 0.075
-            }, index * 1 + 1)
+                    y: '-100%',
+                    duration: 1,
+                    stagger: 0.075
+                }, index * 1 + 1)
             }
 
         });
@@ -77,7 +77,7 @@ export default function Loader(){
             stagger: 0.1,
         })
         tl.call(() => {
-            if(showLoader === true){
+            if (showLoader === true) {
                 const screenLoader = loaderRef.current;
                 screenLoader.style.display = 'none';
                 document.body.style.overflow = 'auto';
@@ -91,38 +91,38 @@ export default function Loader(){
         return () => {
             tl.kill();
         };
-    },[showLoader]);
+    }, [showLoader]);
 
     // Se showLoader for false, o componente não renderiza nada
     if (!showLoader) return null;
 
 
-    return(
+    return (
         <>
             {showLoader && (
                 <div ref={loaderRef} className="fixed top-0 left-0 w-[100vw] h-[100svh] z-20 overflow-hidden">
-            <div className=" absolute top-0 w-full h-full flex">
-                {blocksValue.map((value , index) => (
-                    <div key={index} ref={el => blockRefs.current[index] = el} className="w-full h-full bg-(--main-color-dark) clip"></div>
-                ))}
-            </div>
+                    <div className=" absolute top-0 w-full h-full flex">
+                        {blocksValue.map((value, index) => (
+                            <div key={index} ref={el => blockRefs.current[index] = el} className="w-full h-full bg-(--main-color-dark) clip"></div>
+                        ))}
+                    </div>
 
-            <div className="absolute bottom-1/12 left-1/2 -translate-1/2">
-                <div ref={spinnerRef} className="w-[60px] h-[60px] border-4 border-(--background) rounded-full border-b-transparent animate-spin"></div>
-            </div>
+                    <div className="absolute bottom-1/12 left-1/2 -translate-1/2">
+                        <div ref={spinnerRef} className="w-[60px] h-[60px] border-4 border-(--background) rounded-full border-b-transparent animate-spin"></div>
+                    </div>
 
-            <div ref={counterRef} className="absolute top-1/2 left-1/2 -translate-1/2 z-20">
-                {digitsCount.map((digits, index) => (
-                    <div key={index} ref={el => countRefs.current[index] = el} className="absolute top-1/2 left-1/2 -translate-1/2 flex clip">
-                        {digits.map((d, digitId) => (
-                            <div className="flex flex-1 pt-4 clip" key={digitId} ref={el => digitRefs.current[index * 2 + digitId] = el}>
-                                <h1 className="font-[canoppe] text-[15rem] text-(--background) relative will-change-transform recorte">{d}</h1>
+                    <div ref={counterRef} className="absolute top-1/2 left-1/2 -translate-1/2 z-20">
+                        {digitsCount.map((digits, index) => (
+                            <div key={index} ref={el => countRefs.current[index] = el} className="absolute top-1/2 left-1/2 -translate-1/2 flex clip">
+                                {digits.map((d, digitId) => (
+                                    <div className="flex flex-1 pt-4 clip" key={digitId} ref={el => digitRefs.current[index * 2 + digitId] = el}>
+                                        <h1 className="font-[canoppe] text-[15rem] text-(--background) relative will-change-transform recorte">{d}</h1>
+                                    </div>
+                                ))}
                             </div>
                         ))}
                     </div>
-                ))} 
-            </div>
-        </div>
+                </div>
             )}
         </>
     )
--- src/app/projetos/[projectPage]/page.js
+++ src/app/projetos/[projectPage]/page.js
@@ -1,93 +1,97 @@
-    'use client'
-    import Footer from "@/app/components/Footer";
-    import Navbar from "@/app/components/Navbar";
-    import Link from "next/link";
-    import Image from "next/image";
-    import { ArrowUpRight , GithubIcon, } from "lucide-react";
+'use client'
+import Footer from "@/app/components/Footer";
+import Navbar from "@/app/components/Navbar";
+import Link from "next/link";
+import Image from "next/image";
+import { ArrowUpRight, GithubIcon, } from "lucide-react";
 
-    import { useParams , notFound } from "next/navigation";
-    import { useEffect } from "react";
+import { useParams, notFound } from "next/navigation";
+import { useEffect } from "react";
 
-    import ButtonContactMagnetic from "@/app/components/ButtonContact";
-    import ButtonProject from "@/app/components/ButtonProject";
-    import Content from "@/app/components/content";
-    export default function projectPage() {
-        
-        const reposString = sessionStorage.getItem('githubRepos');
-        const reposArray = reposString ? JSON.parse(reposString) : [];
-        const params = useParams();
-        const slugParam = params.projectPage ?? "";
-        const project = reposArray.find(item => item.slug === slugParam);
+import ButtonContactMagnetic from "@/app/components/ButtonContact";
+import ButtonProject from "@/app/components/ButtonProject";
+import Content from "@/app/components/content";
+export default function projectPage() {
 
-        if(!project){
-            notFound()
-        }
+    const reposString = sessionStorage.getItem('githubRepos');
+    const reposArray = reposString ? JSON.parse(reposString) : [];
+    const params = useParams();
+    const slugParam = params.projectPage ?? "";
+    const project = reposArray.find(item => item.slug === slugParam);
 
-        const getColor = {
-            Figma: 'text-[#E69AFF] border-[#E69AFF]',
-            Next: 'text-[#A89AFF] border-[#A89AFF]',
-            HTML: 'text-[#FA4D4D] border-[#FA4D4D]',
-            Development:'text-[#FADE4D] border-[#FADE4D]',
-            Design: 'text-[#5C45FD] border-[#5C45FD]',
-            Vue:'text-[#68D38A] border-[#68D38A]',
-            Vite:'text-[#68D38A] border-[#68D38A]',
-            React_Native: 'text-[#4595FD] border-[#4595FD]'
-        }
-        return(
-            <section>
-                <Navbar showContact={true} color='var(--foreground)' />
-                <div className="flex flex-col justify-center items-center mt-[15vh] px-5 lg:mt-[20vh]">
-                    <h1 className="font-[canoppe] text-8xl lg:text-9xl text-center text-(--foreground) break-all">{project.nome}</h1>
-                    <div className="flex justify-center items-center font-mono gap-3">
-                        <span className="underline text-sm lg:text-md">{project.situacao} / {project.data}</span>
-                        {project.condicoes.map((item , index) => (
-                            <p key={index} className={`${getColor[item.situacao]} border-2 rounded-full py-1 px-2 font-bold text-sm lg:text-md`}>{item.situacao}</p>
-                        ))}
-                    </div>
-                    <div className="mt-10 mb-4 block lg:hidden">
+    if (!project) {
+        notFound()
+    }
+
+    const getColor = {
+        Figma: 'text-[#E69AFF] border-[#E69AFF]',
+        Next: 'text-[#A89AFF] border-[#A89AFF]',
+        HTML: 'text-[#FA4D4D] border-[#FA4D4D]',
+        Development: 'text-[#FADE4D] border-[#FADE4D]',
+        Design: 'text-[#5C45FD] border-[#5C45FD]',
+        Vue: 'text-[#68D38A] border-[#68D38A]',
+        Vite: 'text-[#68D38A] border-[#68D38A]',
+        React_Native: 'text-[#4595FD] border-[#4595FD]'
+    }
+
+    return (
+        <section>
+            <Navbar showContact={true} color='var(--foreground)' />
+            <div className="flex flex-col justify-center items-center mt-[15vh] px-5 lg:mt-[20vh]">
+                <h1 className="font-[canoppe] text-8xl lg:text-9xl text-center text-(--foreground) break-all">{project.nome}</h1>
+                <div className="flex justify-center items-center font-mono gap-3">
+                    <span className="underline text-sm lg:text-md">{project.situacao} / {project.data}</span>
+                    {project.condicoes.map((item, index) => (
+                        <p key={index} className={`${getColor[item.situacao]} border-2 rounded-full py-1 px-2 font-bold text-sm lg:text-md`}>{item.situacao}</p>
+                    ))}
+                </div>
+                <div className="mt-10 mb-4 block lg:hidden">
+                    {project.urlSite === "" ? (
+                        <div className="bg-(--texte-p-color) px-3 py-1 rounded-2xl mt-8">
+                            <p className="text-md font-mono text-(--background)">Projeto em andamento</p>
+                        </div>
+                    ) : (
+                        <ButtonProject href={project.urlSite || '/'}>
+                            <p className="">visite</p>
+                            <ArrowUpRight size={30} />
+                        </ButtonProject>
+                    )}
+                </div>
+                <div className="mb-30 h-auto lg:h-[600px] w-[85vw] rounded-3xl shadow-md relative">
+                    {/* Imagem Desktop */}
+                    <Image className="hidden lg:block object-cover object-center rounded-3xl lg:mt-10" src={project.src} fill alt="imagem de projeto" />
+                    {/* Imagem Mobile */}
+                    <Image className="block lg:hidden rounded-3xl w-full h-auto" src={project.srcMobile || project.src} width={0} height={0} sizes="100vw" alt="imagem de projeto mobile" />
+                    <div className="absolute top-0 right-0 -translate-2/4 hidden lg:block">
                         {project.urlSite === "" ? (
                             <div className="bg-(--texte-p-color) px-3 py-1 rounded-2xl mt-8">
                                 <p className="text-md font-mono text-(--background)">Projeto em andamento</p>
                             </div>
                         ) : (
-                            <ButtonProject href={project.urlSite || '/'}>
-                                <p className="">visite</p>
+                            <ButtonContactMagnetic href={project.urlSite || '/'} target="_blank">
+                                <p className="text-xl font-[canoppe]">visite</p>
                                 <ArrowUpRight size={30} />
-                            </ButtonProject>
+                            </ButtonContactMagnetic>
                         )}
                     </div>
-                    <div className="mb-30 h-[600] w-[85vw] rounded-3xl shadow-md relative">
-                        <Image className="object-cover object-center rounded-3xl lg:mt-10" src={project.src} fill alt="imagem de projeto"/>
-                        <div className="absolute top-0 right-0 -translate-2/4 hidden lg:block">
-                            {project.urlSite === "" ? (
-                                <div className="bg-(--texte-p-color) px-3 py-1 rounded-2xl mt-8">
-                                    <p className="text-md font-mono text-(--background)">Projeto em andamento</p>
-                                </div>
+                </div>
+                <div className="mx-5 mb-30 xl:mx-48">
+                    <Content text1={project.texto1} text2={project.texto2} />
+                    <div className="flex justify-center py-5 lg:justify-end">
+                        <ButtonProject href={project.urlGit || '/'}>
+                            {project.situacao === 'design' ? (
+                                <p className="">Veja no Behance!</p>
                             ) : (
-                                <ButtonContactMagnetic href={project.urlSite || '/'} target="_blank">
-                                    <p className="text-xl font-[canoppe]">visite</p>
-                                    <ArrowUpRight size={30} />
-                                </ButtonContactMagnetic>
+                                <p className="">Veja no GitHub!</p>
                             )}
-                        </div>
+                        </ButtonProject>
                     </div>
-                    <div className="mx-5 mb-30 xl:mx-48">
-                        <Content text1={project.texto1} text2={project.texto2}/>
-                        <div className="flex justify-center py-5 lg:justify-end">
-                            <ButtonProject href={project.urlGit || '/'}>
-                                {project.situacao === 'design' ? (
-                                    <p className="">Veja no Behance!</p>
-                                ) : (
-                                    <p className="">Veja no GitHub!</p>
-                                )}
-                            </ButtonProject>
-                        </div>
-                    </div>
-                    {/* <div className="bg-[#b1c0b3] w-full px-72 py-30 rounded-t-4xl">
+                </div>
+                {/* <div className="bg-[#b1c0b3] w-full px-72 py-30 rounded-t-4xl">
                         <iframe className="w-full aspect-video" src="https://www.youtube.com/embed/5GyG_ZDaXA4?si=APIzTp6j4aQvTgNr&controls=0&autoplay=1&mute=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen ></iframe>
                     </div> */}
-                </div>
-                <Footer />
-            </section>
-        )
-    }
+            </div>
+            <Footer />
+        </section>
+    )
+}
